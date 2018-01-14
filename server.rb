546f93394da198a87fb4b61ca3b0e7e5451f0e19
James Hartman <jameshartman@Jamess-MacBook-Pro.local>
2018-01-14 18:52:50 -0500

    Added oauth login

--- server.rb
+++ server.rb
@@ -1,5 +1,7 @@
 require "sinatra"
 require 'net/http'
+require 'oauth'
+require 'sinatra/flash'
 
 require "sinatra/reloader" if development?
 require "pry" if development? || test?
@@ -9,11 +11,23 @@ require_relative 'keys'
 set :bind, '0.0.0.0'  # bind to all interfaces
 set :public_folder, File.join(File.dirname(__FILE__), "public")
 
+
 get "/" do
+  @reader = OAuth::Consumer.new(GOODREADS_API_KEY,
+    GOODREADS_OAUTH_SECRET,
+    :site => 'https://www.goodreads.com')
+  @request_token = @reader.get_request_token
+
   erb :home
 end
 
-get "/api/v1/:title" do |title|
-  uri = URI("https://www.goodreads.com/search/index.xml?key=#{GOODREADS_API_KEY}&q=#{title}")
+get '/auth/goodreads/callback' do
+  @access_token = params['oauth_token']
+  binding.pry
+  redirect '/'
+end
+
+get "/api/v1/:title" do
+  uri = URI("https://www.goodreads.com/search/index.xml?key=#{GOODREADS_API_KEY}&q=#{params[:title]}")
   return Net::HTTP.get(uri)
 end
