42d5b7c3dc2abc113ad91e0f66c685196bf24e06
James Hartman <hartman.james.p@gmail.com>
2018-02-08 16:43:29 -0500

    configured dotenv

--- server.rb
+++ server.rb
@@ -3,8 +3,9 @@ require 'net/http'
 
 require "sinatra/reloader" if development?
 require "pry" if development? || test?
+require 'dotenv'
 
-require_relative 'keys'
+Dotenv.load
 
 set :bind, '0.0.0.0'  # bind to all interfaces
 set :public_folder, File.join(File.dirname(__FILE__), "public")
@@ -14,6 +15,6 @@ get "/" do
 end
 
 get "/api/v1/:title" do |title|
-  uri = URI("https://www.goodreads.com/search/index.xml?key=#{GOODREADS_API_KEY}&q=#{title}")
+  uri = URI("https://www.goodreads.com/search/index.xml?key=#{ENV['GOODREADS_API_KEY']}&q=#{title}")
   return Net::HTTP.get(uri)
 end
